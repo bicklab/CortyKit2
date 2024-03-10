73bc788c4b2e38c2122740bf378699571faee146
Robert Corty <rcorty@gmail.com>
2024-03-10 14:50:00 -0500

    trying to get google variables to work

--- R/zzz.R
+++ R/zzz.R
@@ -7,14 +7,18 @@
 #' @export
 #'
 .onLoad = function(libname, pkgname) {
-	assign(
-		"GOOGLE_PROJECT",
-		Sys.getenv('GOOGLE_PROJECT'),
-		envir = parent.env(environment()))
+	GOOGLE_PROJECT <<- Sys.getenv('GOOGLE_PROJECT')
+	GOOGLE_BUCKET <<- Sys.getenv('WORKSPACE_BUCKET')
 
-	assign("GOOGLE_BUCKET",
-				 Sys.getenv('WORKSPACE_BUCKET'),
-				 envir = parent.env(environment()))
+	# assign(
+	# 	"GOOGLE_PROJECT",
+	# 	Sys.getenv('GOOGLE_PROJECT'),
+	# 	envir = parent.env(environment()))
+	#
+	# assign(
+	# 	"GOOGLE_BUCKET",
+	# 	Sys.getenv('WORKSPACE_BUCKET'),
+	# 	envir = parent.env(environment()))
 
 	invisible()
 }
