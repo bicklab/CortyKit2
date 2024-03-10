c58fe40514d4bbe5327324ee44422022fae5def9
Robert Corty <rcorty@gmail.com>
2024-03-10 14:39:43 -0500

    mainly improving onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -7,8 +7,14 @@
 #' @export
 #'
 .onLoad = function(libname, pkgname) {
-	GOOGLE_PROJECT = Sys.getenv('GOOGLE_PROJECT')
-	GOOGLE_BUCKET = Sys.getenv('WORKSPACE_BUCKET')
+	assign(
+		"GOOGLE_PROJECT",
+		Sys.getenv('GOOGLE_PROJECT'),
+		envir = parent.env(environment()))
+
+	assign("GOOGLE_BUCKET",
+				 Sys.getenv('WORKSPACE_BUCKET'),
+				 envir = parent.env(environment()))
 
 	invisible()
 }
