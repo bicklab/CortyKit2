f314ae9be0cc01f05f2aa6fcd80f78bd7d427e39
Robert Corty <robertcorty@mitmh3j2jy962y.dhcp.mc.vanderbilt.edu>
2024-05-06 16:19:38 -0500

    working on binary phewas

--- R/binary_phewas.R
+++ R/binary_phewas.R
@@ -1,12 +1,27 @@
-binary_phewas = function(covars, phecodes, phecode_info, min_num_codes = 4) {
+binary_phewas = function(covars, phecodes, phecode_info,
+												 min_num_codes = 4,
+												 min_num_cases = 5,
+												 num_cores = 18) {
 
+	# cannot function if there are phecodes observed that lack info
 	obs_phecodes = unique(phecodes$phecodeX)
-	if (any(obs_phecodes %nin% phecode_info$phecode)) {
+	if (any(obs_phecodes %nin% phecode_info$phecode))
 		stop('There is at least one phecode that appears in [phecodes] but not in [phecode_info]')
-	}
+
+	# only need info on observed phecodes
 	phecode_info = filter(phecode_info, phecode %in% unique(phecodes$phecodeX))
 
-	results = list()
+	# only use phecode counts that are in great enough quantity
+	# to be believed
+	phecodes = filter(phecodes, phecode_count >= min_num_codes)
+
+	phecode_info |>
+		group_by(category) |>
+		group_split() |>
+		future_map(binary_phewas_)
+
+
+		results = list()
 	for (phecode_idx in 1:nrow(phecode_info)) {
 
 		this_phecode = phecode_info$phecode[phecode_idx]
@@ -22,9 +37,12 @@ binary_phewas = function(covars, phecodes, phecode_info, min_num_codes = 4) {
 			this_covars = covars
 
 		phecodes %>%
-			filter(phecodeX == this_phecode, phecode_count >= min_num_codes) |>
+			filter(phecodeX == this_phecode) |>
 			pull(person_id) ->
 			pids_w_phecode
+
+		if (length(pids_w_phecode) <= min_num_cases)
+			next
 
 		covars %>%
 			mutate(has_phecode = person_id %in% pids_w_phecode) ->
