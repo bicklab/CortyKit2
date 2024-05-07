2902eee2a5f33056b9c583740ff78328b62ec2a7
Robert Corty <rcorty@gmail.com>
2024-05-07 08:58:05 -0500

    improving binary_phewas parallelization

--- R/binary_phewas.R
+++ R/binary_phewas.R
@@ -1,7 +1,7 @@
 #' @title phewas
 #'
 #' @param covars the covariates (predictors) of the logistic glm
-#' @param phecodes table of phecodes
+#' @param phecodes_counts a table of how many times each phecode was observed for each person
 #' @param phecode_info information abou the phecodes
 #' @param min_num_codes minimum number of codes a person must have to count as having the phecode
 #' @param min_num_cases minimum number of cases of a phecode that must be observed to consider it worth testing
@@ -9,36 +9,47 @@
 #' @return a table of results
 #' @export
 binary_phewas = function(covars,
-												 phecodes,
+												 phecode_counts,
 												 phecode_info,
 												 min_num_codes = 4,
 												 min_num_cases = 5) {
 
-	# cannot function if there are phecodes observed that lack info
-	obs_phecodes = unique(phecodes$phecode)
-	if (any(obs_phecodes %nin% phecode_info$phecode))
-		stop('There is at least one phecode that appears in [phecodes] but not in [phecode_info]')
-
-	# only need info on observed phecodes
-	phecode_info = filter(phecode_info, phecode %in% unique(phecodes$phecode))
-
 	# only use phecode counts that are in great enough quantity
 	# to be believed
-	phecodes = filter(phecodes, phecode_count >= min_num_codes)
+	phecode_counts = filter(phecode_counts, phecode_count >= min_num_codes)
+
+	# cannot function if there are phecodes observed that lack info
+	if (any(unique(phecode_counts$phecode) %nin% phecode_info$phecode))
+		stop('There is at least one phecode that appears in [phecode_counts] but not in [phecode_info]')
+
+	# only need info on observed phecodes
+	phecode_info = filter(phecode_info, phecode %in% unique(phecode_counts$phecode))
 
 	# set up multisession evaluation, then split the work across workers
-	future::plan(strategy = multisession)
+	future::plan(future::multisession)
 
 	phecode_info |>
-		dplyr::group_by(category) |>
-		dplyr::group_split() |>
-		furrr::future_map_dfr(binary_phewas_one_chunk) ->
-		result
+		mutate(phecode_prefix = str_sub(phecode, 1, 2)) |>
+		split(~ phecode_prefix) ->
+		phecode_info_split
 
-		return(result)
+	phecode_counts |>
+		mutate(phecode_prefix = str_sub(phecode, 1, 2)) |>
+		split(~ phecode_prefix) ->
+		phecode_counts_split
+
+	return(
+		furrr::future_map2_dfr(
+			.x = phecode_info_split,
+			.y = phecode_counts_split,
+			.f = binary_phewas_one_chunk,
+			covars = covars,
+			.progress = TRUE
+		)
+	)
 }
 
-binary_phewas_one_chunk = function(phecode_info_chunk) {
+binary_phewas_one_chunk = function(phecode_info_chunk, phecodes_chunk, covars) {
 
 	for (phecode_idx in 1:nrow(phecode_info_chunk)) {
 
@@ -54,7 +65,8 @@ binary_phewas_one_chunk = function(phecode_info_chunk) {
 		if (this_sex == 'Both')
 			this_covars = covars
 
-		phecodes %>%
+		# need to pull this into the map somehow!
+		phecodes_chunk %>%
 			filter(phecode == this_phecode) |>
 			pull(person_id) ->
 			pids_w_phecode
@@ -63,10 +75,8 @@ binary_phewas_one_chunk = function(phecode_info_chunk) {
 			next
 
 		covars %>%
-			mutate(has_phecode = person_id %in% pids_w_phecode) ->
-			to_glm
-
-		glm(formula = has_phecode ~ ., data = to_glm) |>
+			mutate(has_phecode = person_id %in% pids_w_phecode) |>
+			glm(formula = has_phecode ~ .) |>
 			broom::tidy() |>
 			mutate(phecode = this_phecode) ->
 			results[[this_phecode]]
